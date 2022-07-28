83270511c1b3da7acb335f9533fe203913012eac
meikalaheshla <fausto.boshoff@gmail.com>
2022-07-28 19:28:26 +0200

    MILESTONE 1 senza css e gusto

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
-    <input type="text"><button>cerca</button>
+    <input type="text" @keyup.enter="getFilmsList" v-model="searchTxt"><button @click="getFilmsList">cerca</button>
     <div>
       <h2>FILM</h2>
       <ul>
-        <li></li>
+        <li v-for="film in films" :key="film.id">
+          <span>Titolo:{{ film.title }}</span><br>
+          <span>Titolo originale:{{ film.original_title }}</span><br>
+          <span>Lingua:{{ film.language }}</span><br>
+          <span>Voto:{{ film.vote_average }}</span><br>
+        </li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 
 
 export default {
@@ -20,9 +26,31 @@ export default {
   }, data() {
     return {
       films: [],
-      searchTxt: "",
+      searchTxt: '',
     }
   }, methods: {
+    fetchFilms() {
+
+    },
+    getFilmsList() {
+      const config = {
+        params: {
+          api_key: '7bcf8d53204c83d72c3d8fe0f87098a1',
+          query: this.searchTxt,
+          language: 'it-IT'
+
+        }
+      }
+      axios
+        .get('https://api.themoviedb.org/3/search/movie', config)
+        .then((res) => {
+          this.films = res.data.results
+        })
+
+
+
+
+    }
 
   }
 
